82c6b850f303608ddfe4fd66873cea9b2e9d3e60
ERNICE778 <erniceken3@gmail.com>
2026-04-18 18:16:05 -0400

    creation de la methode de la classe salle permettant de supprimer les informations d une salle en fonction de son code

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -29,13 +29,20 @@ def insert_salle(self,salle):
 
  
 
-def update_salle(self,salle):
+def update_salle(self, salle ):
         conn= self.get_connection()
         cursor=conn.cursor ()
         cursor.execute(
          "UPDATE  salle SET description= %s,categorie=%s,capacite=%s WHERE  code=%s "  ,
-        (salle.description,salle.categorie,salle.capacite,salle.code)
+        (salle.description,salle.categorie,salle.capacite, salle.code)
         )
         conn.commit()
         conn.close()
 
+def delete_salle(self,code):
+      def delete_salle(self, code):
+        conn=self.get_connection()
+        cursor =conn.cursor ()
+        cursor.execute("DELETE  FROM salle  WHERE code=%s",(code,))
+        conn.commit()
+        conn.close()
